ce3ca1a3d685ada7758a51a99c524a2b6f442cbf
root <tim@thebrookmans.net>
2019-12-01 08:28:32 +0800

    Fixed values and fixed bug with G33 Calibration
    Signed-off-by: root <tim@thebrookmans.net>

--- firmware/Marlin-bugfix-2.0.x/Marlin/Configuration.h
+++ firmware/Marlin-bugfix-2.0.x/Marlin/Configuration.h
@@ -901,7 +901,7 @@
  *
  * Enable this option for a probe connected to the Z Min endstop pin.
  */
-#define Z_MIN_PROBE_USES_Z_MIN_ENDSTOP_PIN
+//#define Z_MIN_PROBE_USES_Z_MIN_ENDSTOP_PIN
 
 /**
  * Z_MIN_PROBE_PIN
@@ -919,7 +919,7 @@
  *      - normally-open switches to 5V and D32.
  *
  */
-//#define Z_MIN_PROBE_PIN 32 // Pin 32 is the RAMPS default
+#define Z_MIN_PROBE_PIN P1_25 // Pin 32 is the RAMPS default
 
 /**
  * Probe Type
